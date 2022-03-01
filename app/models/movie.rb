0c00219b55e67fbccc73b5f958e7052e7a73e345
Harris Sultan <harris_sultan@outlook.com>
2022-02-28 23:22:04 -0500

    commit tree 0a1e90fb42c790ebf1a240e0288b6b3e6ea5d459

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,39 @@
 class Movie < ActiveRecord::Base
-
+    def Movie.create_with_title(title)
+        self.create(title: title)
+    end
+    def Movie.first_movie
+        self.first
+    end
+    def Movie.last_movie
+        self.last
+    end
+    def Movie.movie_count
+        self.count
+    end
+    def Movie.find_movie_with_id(id)
+        self.find(id)
+    end
+    def Movie.find_movie_with_attributes(attributes)
+        self.find_by(attributes)
+    end
+    def Movie.find_movies_after_2002
+        self.where("release_date>2002")
+    end
+    def update_with_attributes(attributes)
+        self.update(attributes)
+    end
+    def Movie.update_all_titles(title)
+        self.update(title: title)
+    end
+    def Movie.delete_by_id(id)
+        movie = self.find(id)
+        movie.destroy
+    end
+    def Movie.delete_all_movies
+        self.destroy_all
+    end
+    def self.find_all_movies_by_year(year)
+        Movie.where(year: year)
+      end
 end
